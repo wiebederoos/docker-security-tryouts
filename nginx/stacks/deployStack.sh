4ef7b5ddecfdeac70d6f7853e219c03fc07cc06a
Wiebe de Roos <w.deroos@flusso.nl>
2017-09-05 21:35:53 +0200

    initial commit of a stack example using docker secrets + env variables to have only 1 definition file for different environments

---
+++ nginx/stacks/deployStack.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# execute this file using . ./deployStack.sh (note the first dot)
+WWW=www1
+
+export LOCAL_VOLUME_PATH=$PWD/$WWW \
+export REMOTE_VOLUME_PATH=/usr/share/nginx/html
+
+docker stack deploy --compose-file docker-compose.yml nginx
